996dd06cc3e647da4e1b3a331085460ad0bd7069
Tom Myrseth <tomyrseth@gmail.com>
2024-04-21 15:51:37 +0200

    added additional stats, bigger sunday recap

--- src/components/Calendar.svelte
+++ src/components/Calendar.svelte
@@ -15,6 +15,8 @@
   export let caloriesData;
   export let dietPlanData;
 
+  console.log(caloriesData);
+
 
   export let uid = 0;
   
@@ -92,11 +94,6 @@
     goto(`/day/?day_id=${day_id}&user_id=${user_id}`);
   }
 
-  function isSunday(date) {
-    let temp = new Date(date);
-    return temp.getDay() === 0;
-  }
-
   function isDateInCurrentOrFuture(dpd, day){
     const dataDate = new Date(dpd.created_at);
     const dataYear = dataDate.getFullYear();
@@ -112,10 +109,97 @@
     if (dayYear < dataYear) return false;
     if (dataYear === dayYear) {
       if (dayMonth < dataMonth) return false;
-      if (dataMonth === dayMonth && dayDay > dataDay) return false;
+      if (dataMonth === dayMonth && dayDay < dataDay) return false;
     }
     return true;
   }
+
+  function isSunday(date) {
+    let temp = new Date(date);
+    return temp.getDay() === 0;
+  }
+
+  function addRecapToCalories() {
+    let sundayList = [];
+    //First filter calories table for uid
+    const filteredUID = caloriesData.filter(item => item.uid === uid);
+
+    //Then get every object that is a sunday
+    for (let i = 0; i < filteredUID.length; i++) {
+      if(isSunday(filteredUID[i].created_at)){
+        sundayList.push(filteredUID[i]);
+      }
+    }
+    //start with first sunday and go back 6 days to calculcate average
+    for (let i = 0; i < sundayList.length; i++) {
+      let recap = weeklyRecap(sundayList[i].created_at);
+      sundayList[i].calorieTot = recap.calories;
+      sundayList[i].proteinTot = recap.protein;
+      sundayList[i].carbsTot = recap.carbs;
+      sundayList[i].fatsTot = recap.fats;
+    }
+
+    //when you have total for that week, merge with original calorie table
+    const additionalDataMap = new Map(sundayList.map(item => [`${item.id}-${item.created_at}`, item]));
+    caloriesData.forEach(item => {
+
+      const key = `${item.id}-${item.created_at}`;
+      if (additionalDataMap.has(key)) {
+        Object.assign(item, additionalDataMap.get(key));
+      }
+    });
+    console.log('MAGIC: ', caloriesData);
+  }
+  addRecapToCalories();
+
+  function weeklyRecap(date) {
+
+    let today = new Date(date);
+    const week = [];
+    const weekFormatted = [];
+    let calories = 0;
+    let protein = 0;
+    let carbs = 0;
+    let fats = 0;
+
+    //put all previous 7 days in an array
+    let k = 0;
+    for (let i = 0; i < 7; i++) {
+      let temp = today.setDate(today.getDate() -k);
+      week.push(new Date(temp));
+      if (k === 0){
+        k = k+1;
+      }
+    }
+    //filter all 7 days into correct format
+    for (let i = 0; i < week.length; i++) {
+      weekFormatted.push(formatDate(week[i]));
+    }
+
+    //get all the dates from caloriesData(supabase) that match the 7 days
+    const filteredUID = caloriesData.filter(item => item.uid === uid);
+    const matchingDates = filteredUID.filter(item => weekFormatted.includes(item.created_at));
+    
+    //extract calories, protein, carbs, fats from them
+    for (let i = 0; i < matchingDates.length; i++) {
+      calories += matchingDates[i].calories;
+      protein += matchingDates[i].protein;
+      carbs += matchingDates[i].carbs;
+      fats += matchingDates[i].fats;
+    }
+    return {calories, protein, carbs, fats};
+  }
+
+  function formatDate(date) {
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1; // getMonth() returns 0-11, add 1 for 1-12
+    const day = date.getDate();
+
+    const formattedMonth = month < 10 ? `0${month}` : `${month}`;
+    const formattedDay = day < 10 ? `0${day}` : `${day}`;
+
+    return `${year}-${formattedMonth}-${formattedDay}`;
+}
 
   async function submitData() {
 
@@ -158,39 +242,60 @@
   {#each emptyStartDays as _, i}
     <div class="dayContainer"></div>
   {/each}
-  
   {#each daysArray as day}
-  <div class={isToday(day.date) ? 'currentDayContainer' : 'dayContainer'}>
+  <div class={isSunday(day.date) ? 'sundayContainer' : 'dayContainer'}>
+
     <button class="dayButton">{day.date.slice(-2)}</button>
-    {#if day.hasTraining}
-      <button class = 'activityButton' on:click={() => openDayPage(day.day_id, day.user_id)}>
-        {day.dayName}
-      </button>
-    {:else}
-      <button class='plusButton' on:click={() => (showModal = true, currentClickedDay = day)}>+</button>
+
+    <div class='mainInfo'>
+      {#if day.hasTraining}
+        <button class = 'activityButton' on:click={() => openDayPage(day.day_id, day.user_id)}>
+          {day.dayName}
+        </button>
+      {:else}
+        <button class='plusButton' on:click={() => (showModal = true, currentClickedDay = day)}>+</button>
+      {/if}
+
+      {#each bwData as bw}
+        {#if bw.created_at === day.date && bw.uid === uid}
+          <p class='bodyweight'>{bw.bodyweight} kg</p>
+        {/if}
+      {/each}
+
+      {#each caloriesData as cal}
+        {#if cal.created_at === day.date && cal.uid === uid}
+          <p class='bodyweight'>{cal.calories} kcal</p>
+        {/if}
+      {/each}
+    </div>
+
+    {#if isSunday(day.date)}
+      <div class="recap">
+        {#each caloriesData as cal}
+          {#if cal.created_at === day.date && cal.uid === uid}
+            {#if cal.calorieTot}
+              {#each dietPlanData as dpd}
+                {#if dpd.uid === uid && isDateInCurrentOrFuture(dpd, day) && isSunday(day.date)}
+                <div class='recap-text'>
+                  <p class='bodyweight'>{dpd.type}</p>
+                  <p class='bodyweight'>{cal.calorieTot} / {dpd.amount} kcal</p>
+                </div>
+                {/if}
+                <div class='bodyweight'> {Math.trunc(cal.calorieTot/7)} / {Math.trunc(dpd.amount/7)} kcal</div>
+                <div class='recap-text'> <p> P: {cal.proteinTot} / {Math.trunc(cal.proteinTot/7)}  g</p> </div>
+                <div class='recap-text'> <p>C: {cal.carbsTot} / {Math.trunc(cal.carbsTot/7)}  g</p> </div>
+                <div class='recap-text'> <p>F: {cal.fatsTot} / {Math.trunc(cal.fatsTot/7)}  g</p> </div>
+              {/each}
+
+            {/if}
+          {/if}
+        {/each}
+      </div>
     {/if}
 
-    {#each bwData as bw}
-      {#if bw.created_at === day.date && bw.uid === uid}
-        <p class='bodyweight'>{bw.bodyweight} kg</p>
-      {/if}
-    {/each}
+    <!-- 
 
-    {#each caloriesData as cal}
-      {#if cal.created_at === day.date && cal.uid === uid}
-        <p class='bodyweight'>{cal.calories} kcal</p>
-      {/if}
-    {/each}
-
-    {#each dietPlanData as dpd}
-      {#if dpd.uid === uid && isDateInCurrentOrFuture(dpd, day) && isSunday(day.date)}
-        <p class='bodyweight'>{dpd.type}</p>
-        <p class='bodyweight'>{dpd.amount}</p>
-      {/if}
-    {/each}
-
-      
-
+    -->
 
     </div>
   {/each}
@@ -241,6 +346,10 @@
     transition: 0.3s;
   }
 
+  .currentDayContainer:hover , .dayContainer:hover {
+    background-color: rgba(26, 26, 26, 0.726);
+  }
+
   .dayContainer {
     display: flex;
     flex-direction: column;
@@ -258,8 +367,28 @@
     transition: 0.3s;
   }
 
-  .currentDayContainer:hover , .dayContainer:hover {
-    background-color: rgba(26, 26, 26, 0.726);
+  .sundayContainer {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    color: rgb(255, 89, 33);
+    height: 150px;
+    width: 300px;
+    padding: 0px;
+    border: solid 2px transparent;
+    border-radius: 5px;
+    background-color: rgb(26, 26, 26);
+    margin: 1px;
+    transition: 0.3s;
+  }
+
+  .recap {
+    display: flex;
+    flex-direction: column;
+    margin-left: 10px;
+    padding: 0;
   }
 
   .dayButton {
